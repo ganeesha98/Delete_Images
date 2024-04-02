11b74e2f45a1ac7bf96c2cc902f6d62220ad5417
Ganeesha Gunathilaka <64259697+ganeesha98@users.noreply.github.com>
2024-04-02 13:37:42 +0530

    updated

---
+++ newdelete/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace newdelete
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button deleteButton; // Button to delete files
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(300, 200);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(189, 47);
+            this.deleteButton.TabIndex = 0;
+            this.deleteButton.Text = "Delete All Files";
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteButton);
+            this.Name = "Form1";
+            this.Text = "File Deletion Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ newdelete/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace newdelete
+{
+    public partial class Form1 : Form
+    {
+        // Default folder path
+        private string folderPath = @"D:\OneDrive - Cargills Ceylon PLC\Desktop\kk";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Calculate the date threshold (30 days ago)
+                DateTime thresholdDate = DateTime.Now.AddDays(-5);
+
+                // Get all files in the folder
+                string[] files = Directory.GetFiles(folderPath);
+
+                // Delete each image file uploaded more than 30 days ago
+                foreach (string file in files)
+                {
+                    if (IsImage(file))
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        // Check if the file creation date is before the threshold date
+                        if (fileInfo.CreationTime < thresholdDate)
+                        {
+                            File.Delete(file);
+                        }
+                    }
+                }
+
+                MessageBox.Show("Old images have been deleted.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        // Helper method to check if a file has an image extension
+        private bool IsImage(string file)
+        {
+            string extension = Path.GetExtension(file).ToLower();
+            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif" || extension == ".bmp";
+        }
+    }
+}
